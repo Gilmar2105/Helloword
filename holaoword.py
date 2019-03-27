12f56f615284321925bb7fbd2bf6d44c2dbd44a4
Gilmar2105 <40172733+Gilmar2105@users.noreply.github.com>
2019-03-27 16:03:00 -0500

    Update holaoword.py

--- holaoword.py
+++ holaoword.py
@@ -1,9 +1,18 @@
 import sys
 import os
 
-nombre = input()
-print("Hola...")
+nombre = input("Cual es tu nombre : ")
+print("Hola ",nombre)
 
+cuenta={}
+
+for caracter in nombre:
+  if caracter in cuenta:
+    cuenta[caracter]=cuenta[caracter]+1
+  else:
+    cuenta[caracter]=1 
+  
+print(cuenta)
 
 
 
